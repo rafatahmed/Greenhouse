faf3721918558cc784da78af2ae2adc13140aaad
Rafat A. Al Khashan <eng.rafatahmed@hotmail.com>
2015-03-04 14:38:56 +0300

    Create Greenhouse_str.py

---
+++ Greenhouse_str.py
@@ -0,0 +1,13 @@
+# The area to be calculated based on the types of Greenhouse - the calculation it will fiollow the car park approche
+
+# 8 Types of Greenhouse 
+ 
+# factores of Dersign 
+
+# Environemntal Study 
+
+# Orient6ation Study by ( Galapgo - Octopus any Genetic Design Theories for fitness ) - 90 degree from the typical north. 
+
+# Design Simulation 
+
+# 
